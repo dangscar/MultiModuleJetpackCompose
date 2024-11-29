e0773d0b85f226c56ac0340f267912c944b7579e
dangrtx <dangrtx@gmail.com>
2024-11-29 09:19:29 +0700

    Setup Multi Module

---
+++ network/src/main/java/com/nlhd/network/TestFile.kt
@@ -0,0 +1,10 @@
+package com.nlhd.network
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TestFile(modifier: Modifier = Modifier) {
+    Text("Hello from the other module")
+}
